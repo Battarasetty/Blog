af5873ec2c8763b31d91be6d1271b5ed8194e596
Battarasetty <harish93601@gmail.com>
2025-01-23 13:15:14 +0530

    Mongodb Connection established add

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3001;
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('connection established successfully');
+    }).catch((err) => {
+        console.log(`something went wrong ${err}`);
+
+    })
+
 
 app.listen(port, () => {
     console.log(`Application connected on ${port}.`);
