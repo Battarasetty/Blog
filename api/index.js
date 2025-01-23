f545e5c3d17ea5aa5705388791977aa3d325c8dd
Battarasetty <harish93601@gmail.com>
2025-01-23 15:41:06 +0530

    rotes and controllers setup add

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -15,7 +16,9 @@ mongoose.connect(process.env.MONGO)
 
     })
 
-
 app.listen(port, () => {
     console.log(`Application connected on ${port}.`);
 })
+
+//Test Route
+app.use('/api/user', userRoutes)
